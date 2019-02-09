a43541ca70db27ff7f0a9094ae6642e6d6cbc7c9
joy <joy.lai@zg.com>
2019-02-09 19:30:06 +0800

    finish

---
+++ go.mod
@@ -0,0 +1,6 @@
+module gitee.com/pefish/p-go-jwt
+
+require (
+	gitee.com/pefish/p-go-error v0.0.0-20190207113842-d80c797909f9
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+)
---
+++ jwt.go
@@ -0,0 +1,53 @@
+package p_jwt
+
+import (
+	"gitee.com/pefish/p-go-error"
+	"github.com/dgrijalva/jwt-go"
+	"time"
+)
+
+type JwtClass struct {
+}
+
+var Jwt = JwtClass{}
+
+func (this *JwtClass) GetJwt(privKey string, expireDuration time.Duration, payload map[string]interface{}) string {
+	token := jwt.New(jwt.GetSigningMethod("RS256"))
+	signKey, err := jwt.ParseRSAPrivateKeyFromPEM([]byte(privKey))
+	claims := make(jwt.MapClaims)
+	claims["exp"] = time.Now().Add(expireDuration).Unix()
+	claims["iat"] = time.Now().Unix() // 颁发时间
+	claims["payload"] = payload
+	token.Claims = claims
+	tokenString, err := token.SignedString(signKey)
+	if err != nil {
+		p_error.ThrowError(`jwt generate error`, 0, err)
+	}
+	return tokenString
+}
+
+func (this *JwtClass) VerifyJwt(pubKey string, tokenStr string) bool {
+	token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+		verifyKey, err := jwt.ParseRSAPublicKeyFromPEM([]byte(pubKey))
+		if err != nil {
+			return nil, err
+		}
+		return verifyKey, nil
+	})
+	if err != nil {
+		p_error.ThrowInternal(`jwt verify error`)
+	}
+	return token.Valid
+}
+
+func (this *JwtClass) DecodeBodyOfJwt(tokenStr string) map[string]interface{} {
+	claims := jwt.MapClaims{}
+	jwt.ParseWithClaims(tokenStr, claims, nil)
+	return claims
+}
+
+func (this *JwtClass) DecodePayloadOfJwtBody(tokenStr string) map[string]interface{} {
+	claims := jwt.MapClaims{}
+	jwt.ParseWithClaims(tokenStr, claims, nil)
+	return claims[`payload`].(map[string]interface{})
+}
